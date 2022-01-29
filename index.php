ea065dbf0a29f42dfc3ba2dbb058c5abc78eb1c8
Calamandes <a@bc.de>
2022-01-29 12:26:41 -0300

    The error backtrace can be testedincluding something like
    // Try to load inexistent file
    require_once 'inexistent_file.php';
    
    in the index.php file

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 require_once 'include/config.php';
+require_once BUSINESS_DIR . 'error_handler.php';
 
+// Set the error handler
+ErrorHandler::SetHandler();
+
+// Load the application page template
 require_once PRESENTATION_DIR . 'application.php';
 
 $application = new Application();
 
-$application->assign('nombre', 'y ahora la wea funciona');
+$application->assign('nombre', 'chages great');
 
 $application->display('index.tpl');
+
--- presentation/index.php
+++ presentation/index.php
@@ -1,15 +1,17 @@
 <?php
-
+apc_clear_cache();
+echo 'WTF';
+require_once 'inexistent_file.php';
 require_once 'include/config.php';
 
 require_once PRESENTATION_DIR . 'application.php';
 
 $application = new Application();
-
+var_dump($application);
 $application->force_compile = true;
 $application->compile_check = true;
 $application->testInstall();
 
-$application->assign('varia', 'Cannibalito');
+$application->assign('nombre', 'Cannibalitox');
 
 $application->display('index.tpl');
